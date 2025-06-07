320fd9f9935aca655b0124c1b0ad21ed448b0187
delikeyki1016 <149133856+delikeyki1016@users.noreply.github.com>
2025-06-07 21:54:40 +0900

    프로필 가져오기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -6,8 +6,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './App.css';
 import Loading from './common/components/Loading';
-import CallbackPage from './pages/CallbackPage';
-// import useExchangeToken from './hooks/useExchangeToken';
+import useExchangeToken from './hooks/useExchangeToken';
+// import CallbackPage from './pages/CallbackPage';
 // lazy loading : 컴포넌트 호출 시에 들고 오겠다.
 const AppLayout = React.lazy(()=> import('./layout/AppLayout'))
 const HomePage = React.lazy(()=> import('./pages/HomePage/HomePage'))
@@ -23,13 +23,22 @@ const PlaylistPage = React.lazy(()=> import('./pages/Playlist/PlaylistPage'))
 // 4. 플레이 리스트 상세 /playlist/:id
 // 5. mobile : playlist view /playlist
 function App() {
-  
+  const urlParams = new URLSearchParams(window.location.search);
+  let code = urlParams.get('code');
+  let codeVerifier = localStorage.getItem("code_verifier");
+  const {mutate: exchangeToken } = useExchangeToken();
+
+  useEffect(()=>{
+    if (code && codeVerifier) {
+      exchangeToken({code, codeVerifier})
+    }
+  },[code, codeVerifier, exchangeToken])
 
   return (
     // Suspense : 컴포넌트 호출 시에 로딩 중 표시
     <Suspense fallback={<Loading />}> 
     <Routes>
-      <Route path="/callback" element={<CallbackPage />} />
+      {/* <Route path="/callback" element={<CallbackPage />} /> */}
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} /> 
         <Route path="search" element={<SearchPage />} />
